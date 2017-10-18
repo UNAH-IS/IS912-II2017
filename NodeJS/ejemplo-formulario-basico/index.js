bb19414669f45bd47d2bb5b2d36cf7ea7ae24fd4
Erick Marín <erick.marin.v@gmail.com>
2017-10-17 22:20:11 -0600

    Ejemplo POST NodeJS

--- NodeJS/ejemplo-formulario-basico/index.js
+++ NodeJS/ejemplo-formulario-basico/index.js
@@ -1,15 +1,31 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+var urlEncodeParser = bodyParser.urlencoded({extended:false});
 app.use(express.static("public"));
+
 app.get("/",function(peticion, respuesta){
 	respuesta.send("Hola mundo");
+});
+
+app.get("/home",function(peticion, respuesta){
+	respuesta.send("ESTE es el resultado de la peticion /home");
 });
 
 app.get("/login",function(peticion, respuesta){
 	var resultado ={
 		usuario:peticion.query.usuario,
 		contrasena:peticion.query.contrasena
+	};
+
+	respuesta.send(JSON.stringify(resultado));
+});
+
+app.post("/login",urlEncodeParser,function(peticion, respuesta){
+	var resultado ={
+		usuario:peticion.body.usuario,
+		contrasena:peticion.body.contrasena
 	};
 
 	respuesta.send(JSON.stringify(resultado));
